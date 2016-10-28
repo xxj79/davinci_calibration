b154ddbe15402d652bbd4422a737c59868f70abe
xxj79 <xxj79@case.edu>
2016-10-28 12:00:37 -0400

    update

--- davinci_kinematics/src/test_pointer.cpp
+++ davinci_kinematics/src/test_pointer.cpp
@@ -4,9 +4,12 @@
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <davinci_traj_streamer/trajAction.h>
-//#include <gsl/gsl_matrix.h>
 #include <iomanip>
-//#include <ginac/ginac.h>
+#include <cmath>
+#include <vector>
+#include <complex>
+
+using namespace std;
 
 bool g_got_new_pose = false;
 int g_count = 0;
@@ -49,13 +52,13 @@ Eigen::Affine3d transformTFToEigen(const tf::Transform &t) {
 
 void move_tip(){
         //g_got_new_pose = true;
-        g_trajectory_point1 = g_trajectory_point2; 
-        g_trajectory_point2.positions[0] += 0.1;
-        g_trajectory_point2.positions[1] += 0.07;
-        g_trajectory_point2.positions[2] += 0.02;
-        g_trajectory_point2.positions[3] += 0.2;
-        g_trajectory_point2.positions[4] += 0.15;
-        g_trajectory_point2.positions[5] += 0.13;
+        g_trajectory_point1 = g_trajectory_point2; //now 40
+        g_trajectory_point2.positions[0] += 0.025;
+        g_trajectory_point2.positions[1] += 0.0175;
+        g_trajectory_point2.positions[2] += 0.005;
+        g_trajectory_point2.positions[3] += 0.05;
+        g_trajectory_point2.positions[4] += 0.0375;
+        g_trajectory_point2.positions[5] += 0.0325;
         g_trajectory_point2.positions[6] += 0;
         //should fix up jaw-opening values...do this later
         g_des_trajectory.header.stamp = ros::Time::now();
@@ -70,12 +73,13 @@ void move_tip(){
 }
 
 void inJointCallback(const sensor_msgs::JointState& jt_msg){
-    g_q_real(0) = jt_msg.position[0]; 
+    g_q_real(2) = jt_msg.position[0]; 
     g_q_real(1) = jt_msg.position[1];
-    g_q_real(2) = jt_msg.position[7];
-    g_q_real(3) = jt_msg.position[8];
+    g_q_real(3) = jt_msg.position[7];
+    g_q_real(6) = jt_msg.position[8];
     g_q_real(4) = jt_msg.position[10];
     g_q_real(5) = jt_msg.position[11];
+    g_q_real(0) = jt_msg.position[12];
 } 
 
 void inPointCallback(const geometry_msgs::Point& pt_msg) {
@@ -251,62 +255,109 @@ int do_inits() {
     
 }
 
+double e(double x, double y, double z, double r, double p, double w){
+    double error=0;
+    for (int i = 0; i<40; i++){
+        error += pow(abs((base(i,0)*cos(w)*cos(p)+base(i,1)*(cos(w)*sin(p)*sin(r)-sin(w)*cos(r))+ base(i, 2)*(cos(w)*sin(p)*cos(r)+sin(r)*sin(w))+x-camera(i, 0))), 2)+ pow(abs((base(i, 0)*sin(w)*cos(p)+base(i, 1)*(sin(w)*sin(p)*sin(r)+cos(w)*cos(r))+ base(i, 2)*(sin(w)*sin(p)*cos(r)-cos(w)*sin(r))+y-camera(i, 1))), 2)+ pow(abs((-base(i, 0)*sin(p)+base(i, 1)*cos(p)*sin(r)+ base(i, 2)*cos(p)*cos(r)+z-camera(i, 2))), 2);
+    }
+    return error;
+}
+
+double x_partial(double x, double y, double z, double r, double p, double w) {
+
+    double x_prime = (e((x + 0.000001),y,z,r,p,w) - e((x - 0.000001), y,z,r,p,w)) / 0.000002; 
+    return x_prime;
+}
+
+double y_partial(double x, double y, double z, double r, double p, double w) {
+
+    double y_prime = (e(x,y + 0.000001,z,r,p,w) - e(x, y - 0.000001,z,r,p,w)) / 0.000002; 
+    return y_prime;
+}
+
+double z_partial(double x, double y, double z, double r, double p, double w) {
+
+    double z_prime = (e(x,y,z + 0.000001,r,p,w) - e(x, y,z - 0.000001,r,p,w)) / 0.000002;
+    return z_prime;
+}
+
+double r_partial(double x, double y, double z, double r, double p, double w) {
+
+    double r_prime = (e(x,y,z,r + 0.000001,p,w) - e(x, y,z,r - 0.000001,p,w)) / 0.000002; 
+    return r_prime;
+}
+
+double p_partial(double x, double y, double z, double r, double p, double w) {
+
+    double p_prime = (e(x,y,z,r,p + 0.000001,w) - e(x, y,z,r,p - 0.000001,w)) / 0.000002; 
+    return p_prime;
+}
+
+double w_partial(double x, double y, double z, double r, double p, double w) {
+
+    double w_prime = (e(x,y,z,r,p,w + 0.000001) - e(x, y,z,r,p,w - 0.000001)) / 0.000002; 
+    return w_prime;
+}
+
+
+void isd(){
+    double tol = 0.000001; // tolerance for convergence
+    int max_iter = 1000000; // maximum number of iterations
+    double dmin = 0.000001;
+    double alpha = 0.01;
+    double gnorm = 100000;
+    double a, b, c;
+    double g = 0; 
+    //double xopt, yopt, zopt, ropt, popt, wopt;
+    double gradx, grady, gradz, gradr, gradp, gradw, x=0, y=0, z=0, r=0, p=0, w=0;
+    //double x0 = 0, y0 =  0, z0 = 0, r0 = 0, p0 = 0, w0 = 0;
+    int niter =0; 
+    double last_fit;
+    double dnorm=100000;
+    double d = 0;
+    //last_fit = e(x0,y0,z0,r0,p0,w0);
+    while (niter<=max_iter && gnorm>=tol && dnorm>=dmin){
+        //a = e(0.000001,0,0,0,0,0) ;
+        //b = e((-0.000001), 0,0,0,0,0) ;                // why????? b = 2a ??? 
+        //c = e(0,0,0,0,0,0);
+        gradx = x_partial(x, y, z, r, p, w);
+        grady = y_partial(x, y, z, r, p, w);
+        gradz = z_partial(x, y, z, r, p, w);
+        gradr = r_partial(x, y, z, r, p, w);
+        gradp = p_partial(x, y, z, r, p, w);
+        gradw = w_partial(x, y, z, r, p, w);
+        x = x - alpha*gradx;
+        y = y - alpha*grady;
+        z = z - alpha*gradz;
+        r = r - alpha*gradr;
+        p = p - alpha*gradp;
+        w = w - alpha*gradw;
+        g = pow(abs(gradx), 2) + pow(abs(grady), 2) + pow(abs(gradz), 2) + pow(abs(gradr), 2) + pow(abs(gradp), 2) + pow(abs(gradw), 2);
+        gnorm = sqrt(g);
+        d = pow(abs(alpha*gradx), 2) + pow(abs(alpha*grady), 2) + pow(abs(alpha*gradz), 2) + pow(abs(alpha*gradr), 2) + pow(abs(alpha*gradp), 2) + pow(abs(alpha*gradw), 2);
+        dnorm = sqrt(d);
+        niter++;
+    }
+
+    
+    cout<<"The transform between base & camera is: " << endl << endl;
+    cout<<"x: "<< x << endl;
+    cout<<"y: "<< y << endl;
+    cout<<"z: "<< z << endl;
+    cout<< "roll: "<< r << endl;
+    cout<< "pitch: "<< p << endl;
+    cout<< "yaw: " << w << endl;    
+
+    cout<< "number of iteration: " << niter << endl;
+    //last_fit = e(x0,y0,z0,r0,p0,w0);
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "test_pointer");
     ros::NodeHandle n;
-/*
-    {gsl_matrix * a = gsl_matrix_alloc( 2, 2);
-    gsl_matrix * b = gsl_matrix_alloc( 2, 2);
+    //ros::Subscriber theJoints = n.subscribe("/davinci/joint_states", 1, inJointCallback);
 
-    gsl_matrix_set_zero(a);
-    gsl_matrix_set_zero(b);
-
-    for(int i=0; i<=1;i++)
-    {
-        for(int j=0; j<= 1 ; j++)
-        {
-            gsl_matrix_set(a,i,j,1.0);
-            gsl_matrix_set(b,i,j,1.0);
-        }
-    }
-
-    gsl_matrix_add(a, b);
-    cout << "matrix a: \n";
-    for(int i=0; i<=1;i++)
-    {
-        for(int j=0; j<= 1 ; j++)
-        {
-            cout << setw(3) << gsl_matrix_get(a,i,j);
-        }
-        cout << "\n";
-    }
-    cout << "matrix b: \n";
-    for(int i=0; i<=1;i++)
-    {
-        for(int j=0; j<= 1 ; j++)
-        {
-            cout << setw(3) << gsl_matrix_get(b,i,j);
-        }
-        cout << "\n";
-    }
-
-    gsl_matrix_free(a);
-    gsl_matrix_free(b);
-}
-*/
-  /*  
-    symbol x("x"), y("y");
-    ex poly;
     
-    for (int i=0; i<3; ++i)
-    poly += factorial(i+16)*pow(x,i)*pow(y,2-i);
-    
-    cout << poly << endl;
-
-*/
-    //ros::Subscriber thePoint = n.subscribe("/thePoint", 1, inPointCallback);
-    //could separately supobscribe to the normal topic, "/the_plane_normal"
-    ros::Subscriber theJoints = n.subscribe("/davinci/joint_states", 1, inJointCallback);
     if (do_inits()!=0) return 1; //poor-man's constructor       
     
      actionlib::SimpleActionClient<davinci_traj_streamer::trajAction> g_action_client("trajActionServer", true);
@@ -332,8 +383,9 @@ int main(int argc, char **argv) {
 
     ROS_INFO("connected to action server"); // if here, then we connected to the server;    
     
-    for (int i=0;i<10;i++){
+    for (int i=0;i<40;i++){
         move_tip();
+        //ros::Subscriber theJoints = n.subscribe("/davinci/joint_states", 1, inJointCallback);
         if (g_got_new_pose) {
             ROS_INFO("sending new goal");
             g_got_new_pose = false;
@@ -353,6 +405,7 @@ int main(int argc, char **argv) {
             }
             ros::Duration(3).sleep();
 
+            ros::Subscriber theJoints = n.subscribe("/davinci/joint_states", 1, inJointCallback);
             //Eigen::Affine3d Davinci_fwd_solver::fwd_kin_solve(const Vectorq7x1& q_vec)
             g_real_gripper1_wrt_base = g_davinci_fwd_solver.fwd_kin_solve(g_q_real); 
             cout<< "the "<< i+1 <<"th point's coord wrt base:"<< g_real_gripper1_wrt_base.translation().transpose() <<endl;
@@ -375,7 +428,6 @@ int main(int argc, char **argv) {
             //The direction of the transform returned will be from the target_frame to the source_frame. 
             //Which if applied to data, will transform data in the source_frame into the target_frame. See tf/CoordinateFrameConventions#Transform_Direction
                     tfListener.lookupTransform("camera", "one_tool_tip_link", ros::Time(0), tfResult);
-            //tfListener.lookupTransform("left_camera_optical_frame", "two_psm_base_link", ros::Time(0), tfResult_two);
                 } catch (tf::TransformException &exception) {
                     ROS_WARN("%s", exception.what());
                     tferr = true;
@@ -388,10 +440,6 @@ int main(int argc, char **argv) {
                 ROS_INFO("tf is good");
             }
             g_affine_camera_to_one_tool_tip = transformTFToEigen(tfResult);
-            //g_point = g_affine_camera_to_one_tool_tip.translation();
-            //pos_in_cam[0][i] = g_point(0);
-            //pos_in_cam[1][i] = g_point(1);
-            //pos_in_cam[2][i] = g_point(2);
             cout<< "the "<< i+1 <<"th point's coord wrt camera:" << g_affine_camera_to_one_tool_tip.translation().transpose() <<endl; 
             for (int j=0;j<3;j++){
                 camera(i,j)= g_affine_camera_to_one_tool_tip.translation().transpose()(j);
@@ -422,12 +470,83 @@ int main(int argc, char **argv) {
             //               ROS_INFO("finished before timeout");
              //          }
             //          ros::Duration(0.5).sleep();
+            ros::spinOnce();
         }
+        //ros::spinOnce();
     }
     cout<< endl;
     cout<< "base dataset:"<< endl;
     cout<< base << endl << endl;
     cout<< "camera dataset:" << endl; 
     cout<< camera << endl << endl;
+    /*
+    base(0,0) = -0.0096675;
+    base(1,0) = -0.012446;
+    base(2,0) = -0.02596;
+    base(3,0) = -0.0364659;
+    base(4,0) = -0.037012;
+    base(5,0) = -0.0251765;
+    base(6,0) = -0.014556;
+    base(7,0) = 0.020327;
+    base(8,0) = 0.027307;
+    base(9,0) = 0.03942;
+
+    base(0,1) = -0.01171;
+    base(1,1) = -0.010012;
+    base(2,1) = 0.002715;
+    base(3,1) = 0.0092682;
+    base(4,1) = 0.0072007;
+    base(5,1) = -0.0046494;
+    base(6,1) = -0.02061;
+    base(7,1) = -0.05972;
+    base(8,1) = -0.056387;
+    base(9,1) = -0.071921;
+
+    base(0,2) = -0.002297;
+    base(1,2) = -0.0009375;
+    base(2,2) = -0.01262;
+    base(3,2) = -0.03381;
+    base(4,2) = -0.0624234;
+    base(5,2) = -0.09182;
+    base(6,2) = -0.11049;
+    base(7,2) = -0.19364;
+    base(8,2) = -0.11681;
+    base(9,2) = -0.12028;
+
+    camera(0,0) = -0.138;
+    camera(1,0) = -0.135;
+    camera(2,0) =-0.121;
+    camera(3,0) =-0.111;
+    camera(4,0) =-0.111;
+    camera(5,0) =-0.124;
+    camera(6,0) =-0.135;
+    camera(7,0) =-0.171;
+    camera(8,0) =-0.179;
+    camera(9,0) =-0.191;
+
+    camera(0,1) =0.06;
+    camera(1,1) =0.058;
+    camera(2,1) =0.045;
+    camera(3,1) =0.039;
+    camera(4,1) =0.042;
+    camera(5,1) =0.055;
+    camera(6,1) =0.072;
+    camera(7,1) =0.11;
+    camera(8,1) =0.108;
+    camera(9,1) =0.124;
+
+    camera(0,2) =0.496;
+    camera(1,2) =0.497;
+    camera(2,2) =0.485;
+    camera(3,2) =0.461;
+    camera(4,2) =0.431;
+    camera(5,2) =0.401;
+    camera(6,2) =0.383;
+    camera(7,2) =0.3;
+    camera(8,2) =0.377;
+    camera(9,2) =0.374;
+*/
+    isd(); 
+    ros::spinOnce();
     return 0; // should never get here, unless roscore dies 
 }
